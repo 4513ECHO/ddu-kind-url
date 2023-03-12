4368dc60aa440d05b5ea322aaeb5607af49c6d54
4513ECHO <4513echo@gmail.com>
2023-03-12 19:13:47 +0900

    feat: Add fetch action as unstable

--- denops/@ddu-kinds/url.ts
+++ denops/@ddu-kinds/url.ts
@@ -7,6 +7,7 @@ import {
   ActionFlags,
   BaseKind,
 } from "https://deno.land/x/ddu_vim@v2.3.0/types.ts";
+import { TextLineStream } from "https://deno.land/std@0.178.0/streams/text_line_stream.ts";
 
 export interface ActionData {
   url?: string;
@@ -14,6 +15,11 @@ export interface ActionData {
 type Params = {
   externalOpener: "openbrowser" | "external";
 };
+interface FetchParams {
+  showHeader?: boolean;
+  body?: string;
+  method?: string;
+}
 
 function getUrl(item: Item): string {
   return (item?.action as ActionData | undefined)?.url ?? item.word;
@@ -63,6 +69,41 @@ export class Kind extends BaseKind<Params> {
         content,
       );
       return ActionFlags.Persist;
+    },
+
+    async fetchUnstable(args) {
+      const params = args.actionParams as FetchParams;
+      for (const item of args.items) {
+        const response = await fetch(getUrl(item), {
+          body: params.body,
+          method: params.method ?? "GET",
+        });
+
+        const header: string[] = [];
+        if (params.showHeader) {
+          header.push(`HTTP/1.1 ${response.status} ${response.statusText}`);
+          for await (const [key, value] of response.headers.entries()) {
+            header.push(`${key}: ${value}`);
+          }
+          header.push("");
+        }
+
+        const body: string[] = [];
+        const bodyRaw = response.body;
+        if (bodyRaw !== null) {
+          const stream = bodyRaw
+            .pipeThrough(new TextDecoderStream())
+            .pipeThrough(new TextLineStream());
+          for await (const line of stream) {
+            body.push(line);
+          }
+        }
+
+        const content = header.concat(body);
+        await args.denops.cmd("new +setlocal\\ buftype=nofile");
+        await args.denops.call("setline", 1, content);
+      }
+      return ActionFlags.None;
     },
   };
 
